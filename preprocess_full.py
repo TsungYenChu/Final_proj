a02005c89d37dd00062e94069de50733a15b4e0c
TsungYenChu <tsungyenchu@gmail.com>
2020-06-25 15:59:31 +0800

    Final version

--- preprocess_full.py
+++ preprocess_full.py
@@ -1,12 +1,12 @@
 #import the module what we used
 import requests
-from pyquery import PyQuery as pq
 import numpy as np
+from pyquery import PyQuery as pq
 import pandas as pd
 import os
 import os.path
-if not os.path.isdir("dataset"):
-    os.mkdir("dataset")
+if not os.path.isdir("dataset_full"):
+    os.mkdir("dataset_full")
 import time
 import datetime
 
@@ -23,8 +23,8 @@ def get_between_month(end_date):
     return month
 
 # add the website with the url into the pyquery
-url = "https://www.sogi.com.tw/"
-html = requests.get(url+"brands/").text
+url = "https://www.sogi.com.tw"
+html = requests.get(url+"/brands/").text
 txt = pq(html)
 
 # Find all of the brands in this site
@@ -43,20 +43,36 @@ brand_list.pop(-2)
 # To pick up the information about cellphone, because this site have the info of tablet and wearable
 for index in range(len(link_list)):
     print("[{:>2d}/{}] {} start.".format(index+1,len(link_list),brand_list[index]))
-    brand = pq(requests.get(url+link_list[index]).text)
+    brand = pq(requests.get(url+link_list[index]).text)("#mixitup-1")
     text =brand.find(".fcellphone").not_('div.mix-item.col-12.col-lg-4.cat1.cat2.cat3.fcellphone.ftablet.fwearable')
     # print(str(brand('#section-list')('#mixitup-1').find(".fcellphone").text()))
     # brand.find(".col-12").find(".d-inline-block").text()
     # brand.find(".fcellphone").not_(".iframe-rwd").text()
     name, price, price_first, price_second, date, month= [],[],[],[],[],[]
     # Find the name of phones
-    pqname = text.find(".text-row-1")+text.find(".text-row-2")
+    pqname = text.find(".text-row-1").not_('div.text-row-1.my-2')+text.find(".text-row-2")
+    label = ["處理器型號","RAM記憶體","ROM儲存空間","記憶卡","最大擴充儲存空間","電池容量","主螢幕尺寸",  \
+             "主螢幕解析度","主螢幕像素密度","主螢幕材質","主相機畫素","主相機光圈F","機身重量","防水防塵等級","3.5mm耳機孔","鏈結"]
+    info = [[] for _ in range(len(label))]
     for i in range(len(pqname)):
+        sub_link = url + pqname.eq(i).attr('href')
+        phone = pq(requests.get(sub_link).text)
+        for tr in phone('tr').items():
+            temp_th = tr('th').eq(0).text()
+            temp_td = tr('td').eq(0).text()
+            if temp_th in label:
+                info[label.index(temp_th)].append(temp_td)
+        info[len(label)-1].append(sub_link)
+        for j in range(len(label)):
+            if len(info[j]) < i+1:
+                info[j].append('')
         name.append(pqname.eq(i).text())
     # Find the price of phones
     pqprice = text.find(".d-block").not_('.d-block text-center').not_('img').not_('a.d-block.text-center')
     for i in [0,1]:
-        price.append(text.find(".d-inline-block").eq(3*i).text()+"\n"+text.find(".d-inline-block").eq(3*i+1).text()+"\n"+text.find(".d-inline-block").eq(3*i+2).text())
+        tmp_price =text.find(".d-inline-block").eq(3*i).text()+"\n"+text.find(".d-inline-block").eq(3*i+1).text()+"\n"+text.find(".d-inline-block").eq(3*i+2).text()
+        if tmp_price != "\n\n":
+            price.append(tmp_price)
     for i in range(len(pqprice)):
         price.append(pqprice.eq(i).text())
     for i in range(len(price)):
@@ -74,15 +90,26 @@ for index in range(len(link_list)):
                 b[-1] = int(b[-1].replace(",",""))
             try:
                 test = datetime.datetime.strptime(c[-1],'%Y/%m')
-                pass
+                price_first.append(a[-1])
+                price_second.append(b[-1])
+                date.append(c[-1])
+                month.append(get_between_month(c[-1]))
+                continue
             except ValueError:
+                if c[-1] == '------':
+                    price_first.append(a[-1])
+                    price_second.append(b[-1])
+                    date.append(np.nan)
+                    month.append(np.nan)
+                    continue
                 test = datetime.datetime.strptime(c[-1],'%Y年%m月')
                 c[-1] = test.strftime('%Y/%m')
-                pass
-            price_first.append(a[-1])
-            price_second.append(b[-1])
-            date.append(c[-1])
-            month.append(get_between_month(c[-1]))
+                price_first.append(a[-1])
+                price_second.append(b[-1])
+                date.append(c[-1])
+                month.append(get_between_month(c[-1]))
+                continue
+
         else:
             a, b = tmp[0].split("$"),tmp[1].split("：")
             if a[-1] =="------":
@@ -91,29 +118,50 @@ for index in range(len(link_list)):
                 a[-1] = int(a[-1].replace(",",""))
             try:
                 test = datetime.datetime.strptime(c[-1],'%Y/%m')
-                pass
+                price_first.append(a[-1])
+                price_second.append(np.nan)
+                date.append(c[-1])
+                month.append(get_between_month(c[-1]))
+                continue
             except ValueError:
+                if c[-1] == '------':
+                    price_first.append(a[-1])
+                    price_second.append(np.nan)
+                    date.append(np.nan)
+                    month.append(np.nan)
+                    continue
                 test = datetime.datetime.strptime(c[-1],'%Y年%m月')
                 c[-1] = test.strftime('%Y/%m')
-                pass
-            price_first.append(a[-1])
-            price_second.append(np.nan)
-            date.append(c[-1])
-            month.append(get_between_month(c[-1]))
+                price_first.append(a[-1])
+                price_second.append(np.nan)
+                date.append(c[-1])
+                month.append(get_between_month(c[-1]))
+                continue
 #     print(len(name),"\n",len(price_first),"\n",len(price_second),"\n",len(date),"\n",len(month))
-    # Combine the name and the price of the phone
-    try:
-        price_first = list(map(int, price_first))
-    except ValueError:
-        pass
-    try:
-        price_second = list(map(int, price_second))
-    except ValueError:
-        pass
-    dic = {"Name":name, "Price":price_first, "Second-hand":price_second, "Listing date":date, "Month":month}
+    dic = {"手機型號":name, "空機價格":price_first, "二手價格":price_second, "上市日期":date, "經過的月份數":month}
+    [dic.update({label[i]:info[i]})for i in range(len(label))]
     df = pd.DataFrame(dic)
     #set up the path
-    path = "./dataset/" + brand_list[index] + ".csv"
+    path = "./dataset_full/" + brand_list[index] + ".csv"
     # save the data as csv
     df.to_csv(path, index = False, header=True)
     print("[{:>2d}/{}] {} complete.".format(index+1,len(link_list),brand_list[index]))
+
+    
+#整合所有csv    
+print("Combine all start.")
+csv_list = []
+for dirPath, dirNames, fileNames in os.walk("./dataset_full/"):
+    for f in fileNames:
+        csv_list.append(os.path.join(dirPath, f))
+
+p = True
+for l_csv in range(len(csv_list)):
+    if p:
+        df1 = pd.read_csv(csv_list[l_csv],",")
+        p = False
+        continue
+    df_tmp= pd.read_csv(csv_list[l_csv],",")
+    df1 = pd.concat([df_tmp,df1],axis=0, ignore_index=True)
+df1.to_csv("./total.csv", index = False, header=True)
+print("Combine all complete.")
